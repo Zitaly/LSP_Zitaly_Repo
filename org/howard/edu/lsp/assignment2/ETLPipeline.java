3342a6d2e7fa31477ae36f77210e286226bde0f1
Zitaly <66892913+Zitaly@users.noreply.github.com>
2025-01-25 13:55:44 -0500

    Added Plans.

---
+++ org/howard/edu/lsp/assignment2/ETLPipeline.java
@@ -0,0 +1,27 @@
+package org.howard.edu.lsp.assignment2;
+
+public class ETLPipeline {
+    
+}
+/*
+ * Needed Info:
+ * 1. Read input files from a relative directory named data.
+ * 2. Read CSV
+ * 2. Transform String and Numbers within the file.
+ * 3. Output a changed file.
+ */
+
+ /*
+  * Cat. 2
+  * Go by commas?
+  */
+
+  /*
+   * Cat. 3
+   * T1. If Category = Electronics; price *= 0.90. Rounded to two decimals.
+   * T2. Ask professor if this is the same as the standardization in that the first letter is capitalized. "NAME" vs "Name"
+   * T3. If Price > 500; Category = Premium Electronics
+   * T4. Same implementation as a grade scale. High to low
+   * T5. Look at chars. First cut down on leading and tailing space. Char 0, find space in middle, then char right after that.
+   *  
+   */
