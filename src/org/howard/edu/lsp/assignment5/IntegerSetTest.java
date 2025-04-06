f9fadef82fdae85dcca45d70c7cc8abf60b98f6c
Zitaly <66892913+Zitaly@users.noreply.github.com>
2025-04-06 17:59:22 -0400

    Got test working.

--- src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
+++ src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
@@ -1,6 +1,10 @@
 package org.howard.edu.lsp.assignment5;
 
-import static org.junit.Test.assertEquals;
+import org.junit.jupiter.api.*;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
 
 public class IntegerSetTest {
     private IntegerSet integerset1;
@@ -8,95 +12,97 @@ public class IntegerSetTest {
 
     @BeforeEach
     public void setUp(){
-        integerset1 = new IntegerSet([1,2,3])
-        integerset2 = new IntegerSet([4,5,6])
+        ArrayList<Integer> set1 = new ArrayList<>(Arrays.asList(1,2,3));
+        ArrayList<Integer> set2 = new ArrayList<>(Arrays.asList(4,5,6));
+        integerset1 = new IntegerSet(set1);
+        integerset2 = new IntegerSet(set2);
     }
 
     @Test
     @DisplayName("Test Clear")
-    public void testClear {
+    public void testClear() {
+        IntegerSet emptyset = new IntegerSet(); // Empty set. Can't compare equals
         integerset1.clear();
         integerset2.clear();
-        assertEquals([], integerset1.toString());
-        assertEquals([], integerset2.toString());
-
+        assertEquals(emptyset, integerset1);
+        assertEquals(emptyset, integerset2);
     }
 
     @Test
     @DisplayName("Test Length")
-    public void testLength {
+    public void testLength() {
         
     }
 
     @Test
     @DisplayName("Test Equals")
-    public void testEquals {
+    public void testEquals() {
         
     }
 
     @Test
     @DisplayName("Test Contains")
-    public void testContains {
+    public void testContains() {
         
     }
 
     @Test
     @DisplayName("Test Largest")
-    public void testLargest {
+    public void testLargest() {
         
     }
 
     @Test
     @DisplayName("Test Smallest")
-    public void testSmallest {
+    public void testSmallest() {
 
     }
 
     @Test
     @DisplayName("Test Add")
-    public void testAdd {
+    public void testAdd() {
         
     }
 
     @Test
     @DisplayName("Test Remove")
-    public void testRemove {
+    public void testRemove() {
         
     }
 
     @Test
     @DisplayName("Test Union")
-    public void testUnion {
+    public void testUnion() {
         
     }
 
     @Test
     @DisplayName("Test Intersect")
-    public void testIntersect {
+    public void testIntersect() {
         
     }
 
     @Test
     @DisplayName("Test Difference")
-    public void testDiff {
+    public void testDiff() {
 
     }
 
     @Test
     @DisplayName("Test Complement")
-    public void testComplement {
+    public void testComplement() {
         
     }
 
     @Test
     @DisplayName("Test Is Empty")
-    public void testIsEmpty {
+    public void testIsEmpty() {
         
     }
 
     @Test
     @DisplayName("Test To String")
-    public void testToString {
+    public void testToString() {
         
     }
 }
