c9162ddf7b5523ab8588c6eafb8279a09dc98846
Zitaly <66892913+Zitaly@users.noreply.github.com>
2025-04-06 17:04:42 -0400

    Added test file. Created function space.

---
+++ src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
@@ -0,0 +1,98 @@
+package org.howard.edu.lsp.assignment5;
+
+import static org.junit.Test;
+
+public class IntegerSetTest {
+    private IntegerSet integerset1;
+    private IntegerSet integerset2;
+
+    @BeforeEach
+    public void setUp(){
+        integerset1 = new IntegerSet([1,2,3])
+        integerset2 = new IntegerSet([4,5,6])
+    }
+
+    @Test
+    @DisplayName("Test Clear")
+    public void testClear {
+
+    }
+
+    @Test
+    @DisplayName("Test Length")
+    public void testLength {
+        
+    }
+
+    @Test
+    @DisplayName("Test Equals")
+    public void testEquals {
+        
+    }
+
+    @Test
+    @DisplayName("Test Contains")
+    public void testContains {
+        
+    }
+
+    @Test
+    @DisplayName("Test Largest")
+    public void testLargest {
+        
+    }
+
+    @Test
+    @DisplayName("Test Smallest")
+    public void testSmallest {
+
+    }
+
+    @Test
+    @DisplayName("Test Add")
+    public void testAdd {
+        
+    }
+
+    @Test
+    @DisplayName("Test Remove")
+    public void testRemove {
+        
+    }
+
+    @Test
+    @DisplayName("Test Union")
+    public void testUnion {
+        
+    }
+
+    @Test
+    @DisplayName("Test Intersect")
+    public void testIntersect {
+        
+    }
+
+    @Test
+    @DisplayName("Test Difference")
+    public void testDiff {
+
+    }
+
+    @Test
+    @DisplayName("Test Complement")
+    public void testComplement {
+        
+    }
+
+    @Test
+    @DisplayName("Test Is Empty")
+    public void testIsEmpty {
+        
+    }
+
+    @Test
+    @DisplayName("Test To String")
+    public void testToString {
+        
+    }
+}
